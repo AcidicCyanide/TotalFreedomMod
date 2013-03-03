7824c9d05df66a871d46ce99fc80562a538ad900
Ryan <Ryan@RYAN-DESKTOP>
2013-03-03 22:30:56 +0000

    Added /food :)

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_food.java
@@ -0,0 +1,22 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Commands.CommandPermissions.ADMIN_LEVEL;
+import me.StevenLawson.TotalFreedomMod.Commands.CommandPermissions.SOURCE_TYPE_ALLOWED;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = ADMIN_LEVEL.SENIOR, source = SOURCE_TYPE_ALLOWED.BOTH, ignore_permissions = false)
+public class Command_food extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        server.dispatchCommand(sender, "cake");
+        server.dispatchCommand(sender, "cookie");
+        server.dispatchCommand(sender, "pie");
+        server.dispatchCommand(sender, "rose");
+
+        return true;
+    }
+}
