4ff1676f0aeda4115921ff92b50dd48323ad0d3c
Ryan <Ryan@RYAN-DESKTOP>
2013-03-03 15:53:29 +0000

    Added Rose

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_rose.java
@@ -0,0 +1,41 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Commands.CommandPermissions.ADMIN_LEVEL;
+import me.StevenLawson.TotalFreedomMod.Commands.CommandPermissions.SOURCE_TYPE_ALLOWED;
+import java.util.Random;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+@CommandPermissions(level = ADMIN_LEVEL.SUPER, source = SOURCE_TYPE_ALLOWED.BOTH, ignore_permissions = true)
+public class Command_rose extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        StringBuilder output = new StringBuilder();
+        Random randomGenerator = new Random();
+
+        String[] words = TotalFreedomMod.CAKE_LYRICS.replaceAll("cake", "rose").split(" ");
+        for (String word : words)
+        {
+            String color_code = Integer.toHexString(1 + randomGenerator.nextInt(14));
+            output.append(ChatColor.COLOR_CHAR).append(color_code).append(word).append(" ");
+        }
+
+        for (Player p : server.getOnlinePlayers())
+        {
+            ItemStack heldItem;
+            heldItem = new ItemStack(Material.RED_ROSE, 1);
+            p.getInventory().setItem(p.getInventory().firstEmpty(), heldItem);
+        }
+
+        TFM_Util.bcastMsg(output.toString());
+        return true;
+    }
+}
