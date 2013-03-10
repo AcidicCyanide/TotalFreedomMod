a4c0802876252f391e1b28543eae6809065e18bf
Ryan <Ryan@RYAN-DESKTOP>
2013-03-10 18:43:56 +0000

    Removed Console //undo stuff

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
@@ -38,7 +38,6 @@ public class Command_gtfo extends TFM_Command
         //Undo WorldEdits:
         if (senderIsConsole)
         {
-            server.dispatchCommand(sender, "gcmd" + p.getName() + "/undo 15");
         }
         else
         {
